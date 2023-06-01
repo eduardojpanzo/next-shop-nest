113fc1e6cadb363e07d1f91b5411784b8e4e9852
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2023-06-01 05:47:35 +0100

    feat: created header

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,32 @@
+import Link from "next/link";
+
+const menuItems = [
+  { path: "/", value: "Home" },
+  { path: "/shop", value: "Shop" },
+  { path: "/about", value: "About Us" },
+  { path: "/contact", value: "Contact Us" },
+];
+
 export function NavBar() {
   return (
-    <div className="bg-slate-300">
-      <div className=" max-w-[1024px] mx-auto p-1 flex items-center justify-around">
+    <div className="bg-slate-100 shadow-md">
+      <div className=" max-w-[1024px] mx-auto px-2 py-4 flex items-center justify-between lg:px-0">
         <ul className="flex gap-3">
-          <li>Shop</li>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
+          {menuItems.map(({ value, path }) => (
+            <li key={value}>
+              <Link
+                className="block w-full h-full px-3 py-2 transition-all hover:text-red-500"
+                href={path}
+              >
+                {value}
+              </Link>
+            </li>
+          ))}
         </ul>
 
         <div className="flex gap-3">
           <button>Criar conta</button>
-          <button>Entrar</button>
+          <button className="text-slate-100 bg-red-500">Entrar</button>
         </div>
       </div>
     </div>
--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,12 +1,39 @@
+import { ShoppingCart, Search } from "lucide-react";
+import Image from "next/image";
+
 export function TopBar() {
   return (
-    <div className="h-[45px] bg-slate-800 ">
-      <div className="h-full max-w-[1024px] flex items-center justify-between mx-auto text-white">
-        <h1>logo</h1>
+    <div className="h-[50px] bg-slate-800 ">
+      <div className="h-full max-w-[1024px] px-2  flex items-center justify-between mx-auto text-white lg:px-0">
+        <h1>
+          <Image
+            src={"/logo-v.png"}
+            alt="logo"
+            width={100}
+            height="42"
+            priority
+          />
+        </h1>
 
-        <input type="search" name="search" id="search" />
+        <form className="w-[35%] min-w-[100px] flex justify-between rounded-sm bg-slate-100 overflow-hidden">
+          <input
+            className=" max-w-[80%] px-2 flex-1 py-1 bg-transparent outline-none text-gray-700"
+            type="search"
+            name="search"
+            id="search"
+            placeholder="Encontre o Melhor Produto"
+          />
+          <button className="px-3 bg-red-500">
+            <Search color="white" size={18} />
+          </button>
+        </form>
 
-        <div>cart</div>
+        <div className="relative w-9 cursor-pointer">
+          <ShoppingCart />
+          <span className="absolute top-0 right-0 flex items-center justify-center w-3 h-3 rounded-full bg-red-500 text-xs font-bold">
+            2
+          </span>
+        </div>
       </div>
     </div>
   );

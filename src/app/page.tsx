6e3c03b96089b192716ba160fe2ac28fbceb26c3
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2023-06-02 16:59:22 +0100

    feat: created a card article

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Banner } from "@/components/Banner";
+import { CategoryCard } from "@/components/CategoryCard";
 
 export default function Home() {
   return (
     <>
       <Banner />
+      <CategoryCard />
     </>
   );
 }

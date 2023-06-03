dbae6b6b2cf5c9ff7720ac058d2faf3cacecba63
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2023-06-03 18:47:38 +0100

    feat: create a product card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import { Banner } from "@/components/Banner";
 import { CategoryCard } from "@/components/CategoryCard";
+import { ProductCard } from "@/components/ProductCard";
+import { Tags } from "@/components/Tags";
 
 export default function Home() {
   return (
     <>
       <Banner />
       <CategoryCard />
+      <ProductCard />
+      <Tags />
     </>
   );
 }
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,3 +1,29 @@
+import Image from "next/image";
+
 export function ProductCard() {
-  return <div>ProductCard</div>;
+  return (
+    <div className="max-w-[300px] flex flex-col border border-slate-300">
+      <div className="max-w-full h-auto">
+        <Image
+          className="w-full object-cover"
+          src={"/product.png"}
+          alt="product"
+          width={300}
+          height={300}
+          priority
+        />
+      </div>
+
+      <div className="w-full h-36 p-3">
+        <h3 className="font-bold text-lg">Product Name</h3>
+        <h4 className="font-bold text-2xl leading-loose">AOA 450.00</h4>
+
+        <div className="flex items-center justify-between text-xs mt-4">
+          <span>Vendido: 40</span>
+
+          <span>disponível : 23</span>
+        </div>
+      </div>
+    </div>
+  );
 }

4c05f5340f3eaba8dfa36897ce3959831dac2dc5
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2023-06-09 19:31:32 +0100

    chore:creat the dialog component

---
+++ src/components/Dialog.tsx
@@ -0,0 +1,38 @@
+import { ReactNode } from "react";
+import * as D from "@radix-ui/react-dialog";
+import { X } from "lucide-react";
+
+type DialogProps = {
+  trigger: ReactNode;
+  children: ReactNode;
+  title: string;
+  desc: string;
+};
+
+export function Dialog({ trigger, title, desc, children }: DialogProps) {
+  return (
+    <D.Root>
+      <D.Trigger asChild>{trigger}</D.Trigger>
+      <D.Portal>
+        <D.Overlay className="bg-blackA9 data-[state=open]:animate-overlayShow fixed inset-0" />
+        <D.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+          <D.Title className="text-mauve12 m-0 text-[17px] font-medium">
+            {title}
+          </D.Title>
+          <D.Description className="text-mauve11 mt-[10px] mb-5 text-[15px] leading-normal">
+            {desc}
+          </D.Description>
+          {children}
+          <D.Close asChild>
+            <button
+              className="text-violet11 hover:bg-violet4 focus:shadow-violet7 absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:shadow-[0_0_0_2px] focus:outline-none"
+              aria-label="Close"
+            >
+              <X />
+            </button>
+          </D.Close>
+        </D.Content>
+      </D.Portal>
+    </D.Root>
+  );
+}

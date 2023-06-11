66dc1b77ffe232be29b235921251eb13c4097561
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2023-06-11 20:27:42 +0100

    chore: use a context

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,8 @@
 "use client";
 
+import { useAuth } from "@/app/hooks/auth";
+import { User } from "lucide-react";
 import Link from "next/link";
-import { useState } from "react";
 import { Dialog } from "./Dialog";
 import { SignInForm } from "./SignInForm";
 import { SignUpForm } from "./SignUpForm";
@@ -14,6 +15,7 @@ const menuItems = [
 ];
 
 export function NavBar() {
+  const { isAuthnticated } = useAuth();
   return (
     <div className="bg-slate-100 shadow-md">
       <div className=" max-w-[1024px] mx-auto px-2 py-4 flex items-center justify-between lg:px-0">
@@ -30,25 +32,31 @@ export function NavBar() {
           ))}
         </ul>
 
-        <div className="flex gap-3">
-          <Dialog
-            title="Criar Conta"
-            desc="Crie a sua conta, ganhe beneficios, favorite os seus produtos e mantenha-se atualizado (a)!"
-            trigger={<button className="bg-slate-300">Criar conta</button>}
-          >
-            <SignUpForm />
-          </Dialog>
+        {isAuthnticated ? (
+          <div className="flex gap-3 cursor-pointer">
+            <User />
+          </div>
+        ) : (
+          <div className="flex gap-3">
+            <Dialog
+              title="Criar Conta"
+              desc="Crie a sua conta, ganhe beneficios, favorite os seus produtos e mantenha-se atualizado (a)!"
+              trigger={<button className="bg-slate-300">Criar conta</button>}
+            >
+              <SignUpForm />
+            </Dialog>
 
-          <Dialog
-            title="Entrar"
-            desc="Entre na sua conta e faça as suas escolhas!"
-            trigger={
-              <button className="text-slate-100 bg-red-500">Entrar</button>
-            }
-          >
-            <SignInForm />
-          </Dialog>
-        </div>
+            <Dialog
+              title="Entrar"
+              desc="Entre na sua conta e faça as suas escolhas!"
+              trigger={
+                <button className="text-slate-100 bg-red-500">Entrar</button>
+              }
+            >
+              <SignInForm />
+            </Dialog>
+          </div>
+        )}
       </div>
     </div>
   );
